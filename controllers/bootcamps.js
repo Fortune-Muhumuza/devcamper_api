0a94454adcdad56cde52d9052c165177a4b62da8
simonpojok <simonpojok@gmail.com>
2023-05-21 00:02:02 +0300

    created all routing routes

---
+++ controllers/bootcamps.js
@@ -0,0 +1,34 @@
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+    res.status(200).json({success: true, message: 'Show all bootcamps'})
+}
+
+// @desc    Get single bootcamp
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, message: 'Create new bootcamp'})
+}
+
+// @desc    Get Create bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, message: `Show bootcamp ${req.params.id}`})
+}
+
+// @desc    Update single bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, message: `Update bootcamp ${req.params.id}`})
+}
+
+// @desc    Delete single bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, message: `Deleted  bootcamp ${req.params.id}`})
+}
